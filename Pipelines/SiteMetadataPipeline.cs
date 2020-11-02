cfb781525c16b20a54603702fe73002e1290f0b8
Petr Švihlík <rocky.intel@gmail.com>
2020-11-02 20:39:19 +0100

    contacts

--- Pipelines/SiteMetadataPipeline.cs
+++ Pipelines/SiteMetadataPipeline.cs
@@ -1,11 +1,12 @@
 ﻿using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.Urls.QueryParameters;
 using Kentico.Kontent.Statiq.Lumen.Models;
 
 namespace Kentico.Kontent.Statiq.Lumen.Pipelines
 {
     public class SiteMetadataPipeline : LoadDataPipeLine<SiteMetadata>
     {
-        public SiteMetadataPipeline(IDeliveryClient deliveryClient) : base(deliveryClient)
+        public SiteMetadataPipeline(IDeliveryClient deliveryClient) : base(deliveryClient, new DepthParameter(2))
         {
         }
     }
